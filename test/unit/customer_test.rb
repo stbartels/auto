88a69448a1b9799415663fe3d6c26dab29da82fc
stbartels <stbartels12@gmail.com>
2011-03-06 15:14:25 -0600

    Added Testing

--- test/unit/customer_test.rb
+++ test/unit/customer_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+  test "customer is invalid without a unique ID" do
+    customer = Customer.new(:customer_id => customers(:bobby).customer_id,
+                            :customer_name => "Testing",
+                            :service_type => "1",
+                            :date => "2011-06-03 11:28:47")
+                            
+    assert !customer.save
+    assert_equal "has already been taken", customer.errors[:customer_id].join("; ")
+  end
   test "the truth" do
     assert true
   end
--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+  test "service is invalid without a unique title" do
+    service = Service.new(:service_type => services(:change_tires).service_type,
+                          :description => "ssss",
+                          :price => 12.97)
+                          
+    assert !service.save
+    assert_equal "has already been take", service.errors[:service_type].join("; ")
+    
+  end
+
   test "the truth" do
     assert true
   end
